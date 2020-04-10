43da12052dcd4ed2fccd6181fd0f102668aea1dd
Jose Antonio <almeida.joseantonio.j@gmail.com>
2020-04-10 16:53:11 -0300

    Dash

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,8 +1,80 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { TouchableOpacity, StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+
+import {
+  Container,
+  Header,
+  Avatar,
+  NameContainer,
+  Welcome,
+  Name,
+  Body,
+  TabContainer,
+  TabBar,
+  Title,
+  Tab,
+  TabText,
+} from './styles';
 
 export default function Dashboard() {
-  return <View />;
+  const dispatch = useDispatch();
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleLogout() {
+    dispatch(signOut());
+  }
+
+  return (
+    <Container>
+      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+      <Header>
+        <Avatar
+          source={{
+            uri: profile.avatar
+              ? profile.avatar.url
+              : `https://avatars.dicebear.com/v2/initials/${profile.name}.svg`,
+          }}
+        />
+        <NameContainer>
+          <Welcome>Bem vindo de volta</Welcome>
+          <Name>{profile.name}</Name>
+        </NameContainer>
+
+        <TouchableOpacity onPress={handleLogout}>
+          <Icon name="exit-to-app" size={24} color="#E74040" size={30} />
+        </TouchableOpacity>
+      </Header>
+
+      <Body>
+        <TabContainer>
+          <Title>Entregas</Title>
+
+          <TabBar>
+            {/* <Overlay style={{ transform: [{ translateX }] }} /> */}
+            <Tab
+            // onPress={() => handleSlide(0, xTabTwo, 'pending')}
+            // onLayout={event => setXTabOne(event.nativeEvent.layout.x)}
+            >
+              <TabText style={{ color: '#7d40e7' }}>Pendentes</TabText>
+            </Tab>
+
+            <Tab
+            // onPress={() => handleSlide(1, xTabOne, 'finish')}
+            // onLayout={event => setXTabOne(event.nativeEvent.layout.x)}
+            >
+              <TabText style={{ color: '#999999' }}>Entregues</TabText>
+            </Tab>
+          </TabBar>
+        </TabContainer>
+      </Body>
+
+      {/* <TopNavigation onChange={setActiveTab} /> */}
+
+      {/* {activeTab === 'pending' ? <Pendings /> : <Deliveries />} */}
+    </Container>
+  );
 }
---
+++ mobile/src/pages/Dashboard/styles.js
@@ -0,0 +1,86 @@
+import styled from 'styled-components/native';
+// import { Platform } from 'react-native';
+import { Animated } from 'react-native';
+
+// import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+// import ShimmerPlaceHolder from 'react-native-shimmer-placeholder';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 20px 20px;
+  background: #fff;
+`;
+
+export const Header = styled.View`
+  height: 138px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Avatar = styled.Image`
+  height: 68px;
+  width: 68px;
+  border-radius: 34px;
+  background: #eee;
+`;
+
+export const NameContainer = styled.View`
+  flex: 1;
+  margin: 0 12px;
+`;
+
+export const Welcome = styled.Text`
+  font-size: 12px;
+  color: #666666;
+`;
+
+export const Name = styled.Text`
+  font-size: 22px;
+  font-weight: bold;
+  color: #444;
+`;
+
+export const Body = styled.View`
+  margin: 0 15px;
+`;
+
+export const TabContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  flex: 1;
+  font-size: 22px;
+  color: #444;
+  font-weight: bold;
+`;
+
+export const TabBar = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: center;
+  position: relative;
+`;
+
+export const Overlay = styled(Animated.View)`
+  position: absolute;
+  height: 10%;
+  width: 50%;
+  background: #7d40e7;
+  bottom: 0;
+  left: 0;
+`;
+
+export const Tab = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  margin-right: 5px;
+`;
+
+export const TabText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  padding: 5px 0;
+`;
